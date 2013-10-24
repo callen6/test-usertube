072188eee5af33cd0c97c058019f338d4a01ed6e
Cori Allen <coriallen@gmail.com>
2013-10-24 06:56:52 -0400

    Add omniauth to users migration, define method on user model, create user on omniauth callbacks controller with logic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,14 @@ class User < ActiveRecord::Base
 
 
   validates_presence_of :username
+  validates_uniqueness_of :username
+
+  def self.from_omniauth(auth)
+	  where(auth.slice(:provider, :uid)).first_or_create do |user|
+	    user.provider = auth.provider
+	    user.uid = auth.uid
+	    user.username = auth.info.name
+	  end
+	end
 
 end
