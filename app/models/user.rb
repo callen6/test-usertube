152183a600da2d1aba6c419694ab890e5c143525
Cori Allen <coriallen@gmail.com>
2013-10-24 07:24:59 -0400

    Alter edit view to remove current password field if user has encrypted password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,4 +36,11 @@ class User < ActiveRecord::Base
     super && provider.blank?
   end
 
+  def update_with_password(params, *options)
+  if encrypted_password.blank?
+    update_attributes(params, *options)
+  else
+    super
+  end
+
 end
