c8b2f5a2b3182f97313c6fe833785771911a977c
Cori Allen <coriallen@gmail.com>
2013-10-23 21:45:18 -0400

    Cleanup loose ends after trying odds and ends to make oauth work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, 
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
- devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
-  validates_presence_of :username, :email
+
+  validates_presence_of :username
 
 end
