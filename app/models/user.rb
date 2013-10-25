c8cca55aaf587cc857f4ac1c3f2422ddec3c19e5
Cori Allen <coriallen@gmail.com>
2013-10-25 16:15:23 -0400

    Working Oauth and Youtube clients.

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,11 +52,13 @@ class User < ActiveRecord::Base
   def youtube_client    
     YouTubeIt::OAuth2Client.new(
       client_access_token: token, 
-      #client_refresh_token: "refresh_token",
       client_id: ENV['CLIENT_ID'], 
       client_secret: ENV['CLIENT_SECRET'], 
-      dev_key: ENV['DEV_KEY'], 
-      expires_at: token_expires_at.to_s)
+      dev_key: ENV['DEV_KEY'],
+      client_refresh_token: refresh_token,
+      lient_token_expires_at: token_expires_at)
+  
   end
+
 
 end
